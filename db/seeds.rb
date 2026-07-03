50540907e2188927631230da1f7e2e6504bc19c9
Natalie-B-Wiese <natalie.b.wiese@gmail.com>
2026-07-02 23:17:09 -0400

    Add data to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,42 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'bcrypt'
+
+def create_user(email:, password:)
+  password_digest=BCrypt::Password.create(password)
+  user=User.find_by(email_address: email, password_digest: password_digest)
+  unless user
+    user=User.create!(email_address: email, password: password, password_confirmation: password) 
+  end
+  user
+end
+
+spiderman=create_user(email: 'spiderman@example.com', password: 'spiders_rock!')
+batman=create_user(email: 'batman@example.com', password: 'batmobile')
+joker=create_user(email: 'joker@example.com', password: 'batman')
+ironman=create_user(email: 'ironman@example.com', password: 'stark')
+
+
+# full started game
+no_clowns_game=Game.find_or_create_by!(name: "No Clowns Allowed Game", player_count: 3, started_at: DateTime.new(2026,6,3,1,1,5))
+Player.find_or_create_by!(user: batman, game: no_clowns_game)
+Player.find_or_create_by!(user: spiderman, game: no_clowns_game)
+Player.find_or_create_by!(user: ironman, game: no_clowns_game)
+
+# not full game (needs 2 more players)
+villains_only=Game.find_or_create_by!(name: "Villains Only Game", player_count: 3)
+Player.find_or_create_by!(user: joker, game: villains_only)
+
+# not full game (needs 1 more player)
+everyone_game=Game.find_or_create_by!(name: "Everyone", player_count: 4)
+Player.find_or_create_by!(user: spiderman, game: everyone_game)
+Player.find_or_create_by!(user: batman, game: everyone_game)
+Player.find_or_create_by!(user: ironman, game: everyone_game)
+Player.find_or_create_by!(user: joker, game: everyone_game)
+
+# full game that is finished where Batman wins
+# TODO: make batman win
+batman_wins_game=Game.find_or_create_by!(name: "Batman vs Joker Game", player_count: 2, started_at: DateTime.new(2001,2,2,1,1,5), ended_at: DateTime.new(2001,2,4,2,2,6))
+Player.find_or_create_by!(user: batman, game: batman_wins_game)
+Player.find_or_create_by!(user: joker, game: batman_wins_game)
