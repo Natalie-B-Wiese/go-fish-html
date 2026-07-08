8690529c1ed1f486337da184aa21f0d393e555fc
Natalie-B-Wiese <natalie.b.wiese@gmail.com>
2026-07-08 09:44:07 -0400

    Make player name save in database when user is created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email_address, :password, :password_confirmation)
+    params.require(:user).permit(:email_address, :name, :password, :password_confirmation)
   end
 end
