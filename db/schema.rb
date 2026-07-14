fc90803df12aaa8fa14e2a288a750521dc0ab309
Natalie-B-Wiese <natalie.b.wiese@gmail.com>
2026-07-14 16:21:10 -0400

    Add archived at to database column

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_14_190621) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_14_201529) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "games", force: :cascade do |t|
+    t.datetime "archived_at"
     t.datetime "created_at", null: false
     t.datetime "ended_at"
     t.jsonb "game_state"
