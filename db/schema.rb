fb1242d726ea34b2ac9f63cd84cc4b442f2e6a97
Natalie-B-Wiese <natalie.b.wiese@gmail.com>
2026-07-15 12:39:26 -0400

    Add country and state to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_14_201529) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_15_163856) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -148,6 +148,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_14_201529) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "address_country"
+    t.string "address_state"
     t.datetime "created_at", null: false
     t.string "email_address", null: false
     t.string "name"
