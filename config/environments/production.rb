cbda299ae2d0ea9bf7c36818618a5c1746a2525b
Natalie-B-Wiese <natalie.b.wiese@gmail.com>
2026-07-17 10:47:17 -0400

    Add logo and icon and setup ability to download

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,10 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
-  # config.assume_ssl = true
+  Rails.application.configure do
+    # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+    config.force_ssl = true
+  end
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
