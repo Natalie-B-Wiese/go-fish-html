718dc7fed2d0238e0b09def6277b4985d1b39258
Natalie-B-Wiese <natalie.b.wiese@gmail.com>
2026-07-17 11:43:43 -0400

    Register a service worker on all pages

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -18,23 +18,24 @@ document.addEventListener('turbo:frame-missing', (event) => {
 })
 
 const registerServiceWorker = async () => {
-    console.log("register service worker called!")
-//   if ("serviceWorker" in navigator) {
-//     try {
-//       const registration = await navigator.serviceWorker.register("/sw.js", {
-//         scope: "/",
-//       });
-//       if (registration.installing) {
-//         console.log("Service worker installing");
-//       } else if (registration.waiting) {
-//         console.log("Service worker installed");
-//       } else if (registration.active) {
-//         console.log("Service worker active");
-//       }
-//     } catch (error) {
-//       console.error(`Registration failed with ${error}`);
-//     }
-//   }
+  if ("serviceWorker" in navigator) {
+    try {
+      const registration = await navigator.serviceWorker.register(  
+        '/service-worker.js',
+        {
+          scope: '/',
+        });
+      if (registration.installing) {
+        console.log("Service worker installing");
+      } else if (registration.waiting) {
+        console.log("Service worker installed");
+      } else if (registration.active) {
+        console.log("Service worker active");
+      }
+    } catch (error) {
+      console.error(`Registration failed with ${error}`);
+    }
+  }
 };
 
 registerServiceWorker();
